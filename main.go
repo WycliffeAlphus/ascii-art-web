305356c50686fb8ca673959cdf475a8ddb90cada
wyonyango <wycliffealphus7@gmail.com>
2024-07-23 16:51:05 +0300

    feat: add the main file

--- main.go
+++ main.go
@@ -1,3 +1,17 @@
 package main
 
-func main(){}
+import (
+	"fmt"
+	"net/http"
+	"ascii-art-web/ascii-art"
+)
+
+func main() {
+	http.HandleFunc("/", asciiart.ServeIndexPage)
+	http.HandleFunc("/ascii-art", asciiart.FormHandler)
+
+	fmt.Println("Starting server on :8081")
+	if err := http.ListenAndServe(":8081", nil); err != nil {
+		fmt.Println("Error starting server:", err)
+	}
+}

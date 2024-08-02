97742d9abad51587fe3691a3ca8fa456b5579a8e
wyonyango <wycliffealphus7@gmail.com>
2024-08-02 17:43:03 +0300

    feat:update main.go

--- main.go
+++ main.go
@@ -8,7 +8,8 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", asciiart.FormHandler)
+	http.HandleFunc("/", asciiart.GetHandler)
+	http.HandleFunc("/ascii-art",asciiart.FormHandler)
 
 	fmt.Println("Starting server on :http://localhost:8081")
 	if err := http.ListenAndServe(":8081", nil); err != nil {

9b9efa27129badb566432ad4a51fcb7eeb6feee3
wyonyango <wycliffealphus7@gmail.com>
2024-08-05 12:28:16 +0300

    fix:add error handling for arguments

--- main.go
+++ main.go
@@ -3,13 +3,18 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 
 	asciiart "ascii-art-web/handler"
 )
 
 func main() {
+	if len(os.Args) != 1 {
+		fmt.Println("encountered error\nUsage: go run . or go run <name of program>")
+		return
+	}
 	http.HandleFunc("/", asciiart.GetHandler)
-	http.HandleFunc("/ascii-art",asciiart.FormHandler)
+	http.HandleFunc("/ascii-art", asciiart.FormHandler)
 
 	fmt.Println("Starting server on :http://localhost:8081")
 	if err := http.ListenAndServe(":8081", nil); err != nil {
